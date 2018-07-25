53efdf50f1b5acb01157e73a07ecb46711764f99
Patrick Lehmann <patlehmann1@gmail.com>
2018-07-25 19:31:12 -0400

    adding functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,18 +1,14 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require('console.table');
+const divider = "\n------------------------------------------------------------------------------------------";
 
+const idNumberArray = [];
 
 const connection = mysql.createConnection({
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "B!gRed1988",
     database: "bamazon"
 });
@@ -20,37 +16,62 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    readProducts("SELECT * FROM products");
+    readAllProducts();
 });
 
-function readProducts(input) {
-    console.log("WELCOME TO BAMAZON!\n");
-    connection.query(input, function (err, res) {
+function readAllProducts() {
+    console.log(divider + "\nWELCOME TO BAMAZON!\n" + divider);
+    connection.query("SELECT * from products", function (err, res) {
         if (err) throw err;
+        for (i = 0; i < res.length; i++) {
+            idNumberArray.push(res[i].item_id);
+        }
         console.table(res);
-        findItem();
+        console.log(divider);
+        singleItemChoice();
     });
 }
 
-function findItem() {
+function singleItemChoice() {
     inquirer.prompt([
         {
             type: "input",
             name: "idQuery",
             message: "What is the ID of the product you would like to buy?"
         }]).then(answers => {
-            if (isNaN(answers.idQuery)) {
-                console.log("Please input a number!");
-                findItem();
-            } else if (answers.idQuery > 1000 && answers.idQuery < answers[answers.length - 1]) {
-                console.log("I will come back and finish this at a later date");
-                connection.end();
-                // Will add function to select the ID and display just that ID's information...
+            if (!isNaN(answers.idQuery)) {
+                const numberValue = parseInt(answers.idQuery);
+                const isInArray = idNumberArray.includes(numberValue);
+                if (isInArray) {
+                    console.log("\n\nHere is your item!" + divider);
+                    connection.query("SELECT * from products WHERE item_id = " + answers.idQuery, function (err, res) {
+                        if (err) throw err;
+                        console.table(res);
+                        console.log(divider);
+                        quantityChoice();
+                    });
+                } else {
+                    console.log("\n\nThat ID does not match a product in our system. Please try again.\n\n");
+                    singleItemChoice();
+                }
+            } else {
+                console.log("\n\nBamazon only uses numbers to identify their products. Please try again.\n\n");
+                singleItemChoice();
             }
-            // this code isn't working, need to figure this out in the morning...
-            // if (answers.idQuery < 1000 && answers.idQuery > answers[answers.length - 1]) {
-            //     console.log("That ID number does not match an item! Try again!");
-            //     findItem();
-            // }
         });
+}
+
+function quantityChoice() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "quantityQuery",
+            message: "How many of this item would you like?"
+        }
+    ]).then(answers => {
+        console.log(divider);
+        //obviously a joke, will add more later.. 
+        console.log("\nI DONT CARE THAT YOU WANT " + answers.quantityQuery + " OF THIS PRODUCT!!!");
+        connection.end();
+    });
 }
